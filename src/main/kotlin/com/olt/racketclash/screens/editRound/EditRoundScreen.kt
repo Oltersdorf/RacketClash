1f8831c0d4de9ed6fe0a72180962b0143dc253c9
Felix Oltersdorf <felix.oltersdorf@gmail.com>
2024-03-17 14:14:38 +0100

    Added a dropdown menu to EditRoundScreen for selecting different types of formats

--- src/main/kotlin/com/olt/racketclash/screens/editRound/EditRoundScreen.kt
+++ src/main/kotlin/com/olt/racketclash/screens/editRound/EditRoundScreen.kt
@@ -1,10 +1,11 @@
 package com.olt.racketclash.screens.editRound
 
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,7 +13,9 @@ import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import com.olt.racketclash.navigation.Screens
 import com.olt.racketclash.ui.TournamentScaffold
 import com.olt.racketclash.ui.TournamentTabs
 
@@ -29,48 +32,147 @@ class EditRoundScreen(private val modelBuilder: () -> EditRoundModel) : Screen {
             selectedTab = TournamentTabs.Games,
             navigateTo = screenModel::navigateTo
         ) {
-            EditRoundView(paddingValues = it, addRound = screenModel::addRound)
+            EditRoundView(paddingValues = it, addRound = screenModel::addRound, navigateTo = screenModel::navigateTo)
+        }
+    }
+}
+
+private sealed class RoundType(val name: String) {
+    data object Empty : RoundType("Empty")
+    data object EquallyDouble : RoundType("Equally strong doubles")
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun EditRoundView(
+    paddingValues: PaddingValues,
+    addRound: (String) -> Unit,
+    navigateTo: (Screens, Navigator) -> Unit
+) {
+    Surface(
+        modifier = Modifier.fillMaxSize().padding(paddingValues = paddingValues)
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            val verticalScrollState = rememberScrollState()
+
+            Column(
+                modifier = Modifier
+                    .requiredWidthIn(500.dp)
+                    .fillMaxWidth(0.5f)
+                    .verticalScroll(verticalScrollState),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                var name by remember { mutableStateOf("") }
+
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = name,
+                    onValueChange = { name = it },
+                    label = { Text("Name") }
+                )
+
+                var expanded by remember { mutableStateOf(false) }
+                var selectedRoundType by remember { mutableStateOf<RoundType>(RoundType.Empty) }
+                ExposedDropdownMenuBox(
+                    modifier = Modifier.padding(top = 50.dp, bottom = 50.dp),
+                    expanded = expanded,
+                    onExpandedChange = { expanded = !expanded }
+                ) {
+                    TextField(
+                        modifier = Modifier.menuAnchor().fillMaxWidth(),
+                        readOnly = true,
+                        value = selectedRoundType.name,
+                        onValueChange = {},
+                        label = { Text("Team") },
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) }
+                    )
+
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        listOf(RoundType.Empty, RoundType.EquallyDouble).forEach {
+                            DropdownMenuItem(
+                                text = { Text(text = it.name) },
+                                onClick = {
+                                    selectedRoundType = it
+                                    expanded = false
+                                },
+                                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                            )
+                        }
+                    }
+                }
+
+                when (selectedRoundType) {
+                    RoundType.Empty -> {}
+                    RoundType.EquallyDouble -> EquallyStrongDouble()
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    val navigator = LocalNavigator.currentOrThrow
+
+                    Spacer(modifier = Modifier.weight(1.0f))
+                    Button(onClick = { navigateTo(Screens.Games, navigator) }) {
+                        Text("Cancel")
+                    }
+                    Button(onClick = {
+                        addRound(name)
+                        navigateTo(Screens.Games, navigator)
+                    }) {
+                        Text("Save")
+                    }
+                }
+            }
+
+            VerticalScrollbar(
+                modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+                adapter = rememberScrollbarAdapter(scrollState = verticalScrollState),
+                style = LocalScrollbarStyle.current.copy(hoverColor = MaterialTheme.colorScheme.secondary, unhoverColor = MaterialTheme.colorScheme.secondary)
+            )
         }
     }
 }
 
 @Composable
-private fun EditRoundView(
-    paddingValues: PaddingValues,
-    addRound: (String) -> Unit
-) {
-    Surface(
-        modifier = Modifier.fillMaxSize().padding(paddingValues = paddingValues)
+private fun EquallyStrongDouble() {
+    Column(
+        modifier = Modifier.padding(bottom = 50.dp).fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
     ) {
-        Column(
-            modifier = Modifier.requiredWidthIn(500.dp).fillMaxWidth(0.5f),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            var name by remember { mutableStateOf("") }
-
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = name,
-                onValueChange = { name = it },
-                label = { Text("Name") }
-            )
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(10.dp)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            FilledIconButton(
+                onClick = {},
+                enabled = true
             ) {
-                val navigator = LocalNavigator.currentOrThrow
-
-                Spacer(modifier = Modifier.weight(1.0f))
-                Button(onClick = { navigator.pop() }) {
-                    Text("Cancel")
-                }
-                Button(onClick = {
-                    addRound(name)
-                    navigator.pop()
-                }) {
-                    Text("Save")
-                }
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    contentDescription = "Remove"
+                )
             }
+            Text("1")
+            FilledIconButton(
+                onClick = {},
+                enabled = true
+            ) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowRight,
+                    contentDescription = "Add"
+                )
+            }
+            Text(text = "Rounds")
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(
+                modifier = Modifier.padding(start = 0.dp),
+                checked = false,
+                onCheckedChange = {}
+            )
+            Text("Different partners each round")
         }
     }
 }
