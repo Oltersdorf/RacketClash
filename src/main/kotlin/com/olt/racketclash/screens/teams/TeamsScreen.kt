09f8f0d4c3a9ecdb42622ffbeb1a95360c459150
Felix Oltersdorf <felix.oltersdorf@gmail.com>
2024-03-19 20:39:52 +0100

    Removed unused imports

--- src/main/kotlin/com/olt/racketclash/screens/teams/TeamsScreen.kt
+++ src/main/kotlin/com/olt/racketclash/screens/teams/TeamsScreen.kt
@@ -1,6 +1,5 @@
 package com.olt.racketclash.screens.teams
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -10,7 +9,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
