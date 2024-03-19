75d038b34a67547a0f912b2f592579921c32d42b
Felix Oltersdorf <felix.oltersdorf@gmail.com>
2024-03-19 16:33:54 +0100

    Changed Surface to material3

--- src/main/kotlin/com/olt/racketclash/screens/editRound/EditRoundScreen.kt
+++ src/main/kotlin/com/olt/racketclash/screens/editRound/EditRoundScreen.kt
@@ -1,8 +1,8 @@
 package com.olt.racketclash.screens.editRound
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Surface
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
