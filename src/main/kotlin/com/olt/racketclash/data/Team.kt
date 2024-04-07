0ad20d5514d8674625d1eae2f5f859d386077e77
Felix Oltersdorf <felix.oltersdorf@gmail.com>
2024-04-07 17:03:28 +0200

    Added statistics to team

--- src/main/kotlin/com/olt/racketclash/data/Team.kt
+++ src/main/kotlin/com/olt/racketclash/data/Team.kt
@@ -4,5 +4,14 @@ data class Team(
     val id: Long,
     val name: String,
     val strength: Int,
-    val size: Int
+    val size: Int,
+    val openGames: Int = 0,
+    val bye: Int = 0,
+    val played: Int = 0,
+    val wonGames: Int = 0,
+    val lostGames: Int = 0,
+    val wonSets: Int = 0,
+    val lostSets: Int = 0,
+    val wonPoints: Int = 0,
+    val lostPoints: Int = 0
 )
--- src/main/kotlin/com/olt/racketclash/database/mapper/TeamMapper.kt
+++ src/main/kotlin/com/olt/racketclash/database/mapper/TeamMapper.kt
@@ -4,4 +4,18 @@ import com.olt.racketclash.data.Team
 import com.olt.racketclash.database.team.SelectAll
 
 fun SelectAll.toTeam() =
-    Team(id = id, name = name, strength = strength, size = size.toInt())
+    Team(
+        id = id,
+        name = name,
+        strength = strength,
+        size = size.toInt(),
+        openGames = openGames?.toInt() ?: 0,
+        bye = bye?.toInt() ?: 0,
+        played = played?.toInt() ?: 0,
+        wonGames = wonGames?.toInt() ?: 0,
+        lostGames = lostGames?.toInt() ?: 0,
+        wonSets = wonSets?.toInt() ?: 0,
+        lostSets = lostSets?.toInt() ?: 0,
+        wonPoints = wonPoints?.toInt() ?: 0,
+        lostPoints = lostPoints?.toInt() ?: 0
+    )
