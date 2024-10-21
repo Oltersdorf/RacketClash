311d165bd61968098262b5eb5b96f146051f2a91
Felix Oltersdorf <felix.oltersdorf@gmail.com>
2024-10-21 17:37:51 +0200

    Added Cache

---
+++ common/database/src/main/kotlin/com/olt/racketclash/database/cache/Cache.kt
@@ -0,0 +1,42 @@
+package com.olt.racketclash.database.cache
+
+internal class Cache<T>(
+    private val size: Int,
+    private val cache: MutableList<T> = mutableListOf()
+) {
+    init {
+        require(size > 0) { "Size must be greater than 0" }
+    }
+
+    fun find(selector: (T) -> Boolean): Result<T> {
+        val cacheResult = cache.find(selector)
+        return if (cacheResult != null)
+            Result.Hit(value = cacheResult)
+        else
+            Result.Miss
+    }
+
+    fun add(value: T) {
+        if (cache.size == size)
+            cache.removeFirst()
+
+        cache.add(value)
+    }
+
+    fun findOrAdd(selector: (T) -> Boolean, valueFactory: () -> T): T {
+        val result = find(selector)
+
+        if (result is Result.Hit)
+            return result.value
+        else {
+            val value = valueFactory()
+            add(value)
+            return value
+        }
+    }
+
+    sealed class Result<out T> {
+        data class Hit<T>(val value: T) : Result<T>()
+        data object Miss : Result<Nothing>()
+    }
+}
