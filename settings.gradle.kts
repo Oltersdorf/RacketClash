40b8f87025eccb9377c10f59a10d89405534a253
Felix Oltersdorf <felix.oltersdorf@gmail.com>
2024-10-26 04:13:42 +0200

    Added new module UI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "RacketClash"
-include("language", "database", "app:desktop", "app:state", "app:ui", "util:state", "util:ui", "common:database")
+include("common:database", "common:ui", "desktop")
