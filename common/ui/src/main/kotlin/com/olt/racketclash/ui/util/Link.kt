1d0e9a45d7b8ec8a46eab052a59f8171dd8c1e89
Felix Oltersdorf <felix.oltersdorf@gmail.com>
2024-10-26 04:14:20 +0200

    Add Link builder to UI

---
+++ common/ui/src/main/kotlin/com/olt/racketclash/ui/util/Link.kt
@@ -0,0 +1,25 @@
+package com.olt.racketclash.ui.util
+
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.unit.TextUnit
+
+@Composable
+fun Link(
+    text: String,
+    fontSize: TextUnit,
+    onClick: () -> Unit
+) {
+    ClickableText(
+        text = AnnotatedString(text = text, spanStyle = SpanStyle(color = Color.Blue, fontSize = fontSize)),
+        modifier = Modifier.pointerHoverIcon(icon = PointerIcon.Hand)
+    ) {
+        onClick()
+    }
+}
