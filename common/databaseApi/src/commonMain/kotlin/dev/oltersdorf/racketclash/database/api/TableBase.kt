86055ec4811d826cb56dbb358eecf6d409c64625
Felix Oltersdorf <felix.oltersdorf@gmail.com>
2026-02-01 12:58:34 +0100

    Added return types for functions in TableBase

--- common/databaseApi/src/commonMain/kotlin/dev/oltersdorf/racketclash/database/api/TableBase.kt
+++ common/databaseApi/src/commonMain/kotlin/dev/oltersdorf/racketclash/database/api/TableBase.kt
@@ -1,11 +1,11 @@
 package dev.oltersdorf.racketclash.database.api
 
 interface TableBase<Item: IdItem, Filter, Sorting> {
-    suspend fun insert(data: Item)
+    suspend fun insert(data: Item): Long
 
-    suspend fun delete(id: Long)
+    suspend fun delete(id: Long): Boolean
 
-    suspend fun update(data: Item)
+    suspend fun update(data: Item): Boolean
 
     suspend fun selectSingle(id: Long): Item?
 
